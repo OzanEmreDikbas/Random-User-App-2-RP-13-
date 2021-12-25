9f69c1a3bcb683d6f5607b1bf39cb54364cb47f1
Ozan Emre Dikbaş <87207946+OzanEmreDikbas@users.noreply.github.com>
2021-12-25 21:27:06 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { Button, Card, Col, Image, Row } from "react-bootstrap";
+import "./App.css";
+import Icon from "./components/Icon";
+import mail from "../src/assets/mail.svg";
+function App() {
+  return (
+    <div className="App">
+      <Card className="text-center">
+        <Card.Header className="header"></Card.Header>
+        <Row>
+          <Col>
+            <Image src={mail} className="user" roundedCircle />
+          </Col>
+        </Row>
+        <Card.Body>
+          <Card.Title>Special title treatment</Card.Title>
+          <Card.Text>
+            With supporting text below as a natural lead-in to additional
+            content.
+          </Card.Text>
+          <Icon />
+          <div className="btn">
+            <Button variant="primary">Go somewhere</Button>
+          </div>
+          <div className="btn">
+            <Button variant="primary">Go somewhere</Button>
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
+export default App;
